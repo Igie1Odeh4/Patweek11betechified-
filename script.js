2df47075d120f10fadded93b9562e366a6d939c0
Erhauyi Patrick Omokpaogbee <pomokpaogbee@corp.aomfb.local>
2026-05-22 16:40:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Mobile Navbar Toggle
+const navLinks = document.querySelector(".nav-links");
+const openBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+const navItems = document.querySelectorAll(".nav-links a");
+
+// OPEN MENU
+openBtn.addEventListener("click", () => {
+  navLinks.classList.add("active");
+
+  openBtn.style.display = "none";
+  closeBtn.style.display = "block";
+
+  document.body.style.overflow = "hidden"; // prevent background scroll
+});
+
+// CLOSE MENU
+closeBtn.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+
+  openBtn.style.display = "block";
+  closeBtn.style.display = "none";
+
+  document.body.style.overflow = "auto";
+});
+
+// AUTO-CLOSE WHEN CLICKING A LINK
+navItems.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+
+    openBtn.style.display = "block";
+    closeBtn.style.display = "none";
+
+    document.body.style.overflow = "auto";
+  });
+});
+
+const skillItems = document.querySelectorAll(".skill-item");
+
+skillItems.forEach((item) => {
+  const head = item.querySelector(".head");
+  const content = item.querySelector(".items");
+  const icon = item.querySelector(".head i.fa-arrow-down");
+
+  head.addEventListener("click", () => {
+
+    const isOpen = content.style.maxHeight;
+
+    // close all
+    document.querySelectorAll(".items").forEach(el => {
+      el.style.maxHeight = null;
+    });
+
+    document.querySelectorAll(".fa-arrow-down").forEach(el => {
+      el.style.transform = "rotate(0deg)";
+    });
+
+    // toggle current
+    if (!isOpen) {
+      content.style.maxHeight = content.scrollHeight + "px";
+      icon.style.transform = "rotate(180deg)";
+    } else {
+      content.style.maxHeight = null;
+      icon.style.transform = "rotate(0deg)";
+    }
+
+  });
+});
